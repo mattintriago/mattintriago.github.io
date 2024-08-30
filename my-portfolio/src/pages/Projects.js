3589b8bef30805b4d5dc56611caf48c58d65ea82
Matt Intriago <mintriago2017@my.fit.edu>
2024-08-29 22:19:21 -0400

    fix dumb mistake.

--- my-portfolio/src/pages/Projects.js
+++ my-portfolio/src/pages/Projects.js
@@ -53,7 +53,7 @@ const Projects = () => {
       id: "debris-removal",
       title: "Debris in Space Autonomous Removal Mechanism",
       description: "Acted as a Control Systems Engineer in developing a single-use, autonomous, and universal space debris grappling device that uses capacitor discharge stud welding as its method of attachment. As a part of my role, I was responsible for developing a simulation model to demonstrate the device's ability to capture debris in space using ROS and Gazebo.",
-      githubLink: "http://localhost:3000/sp",
+      githubLink: "http://mattintriago.github.io/sp",
       demoLink: "https://www.youtube.com/watch?v=66Hwhugrm3c&t=4s"
     },
     {
